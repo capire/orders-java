252e67cd6c981324b0d116ce3addd96b621657dc
Johannes Vogt <akatuoro6@gmail.com>
2026-01-23 16:51:17 +0100

    Review (#2)
    * simplify change detection
    * simplify delete handler

--- srv/src/main/java/customer/orders/OrdersHandler.java
+++ srv/src/main/java/customer/orders/OrdersHandler.java
@@ -1,80 +1,65 @@
 package customer.orders;
 
-import com.sap.cds.services.ServiceException;
-import com.sap.cds.services.ErrorStatuses;
 import com.sap.cds.services.cds.CqnService;
 import org.springframework.stereotype.Component;
 
-import com.sap.cds.services.cds.CdsDeleteEventContext;
 import com.sap.cds.services.EventContext;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.ServiceName;
 import com.sap.cds.services.handler.annotations.Before;
-
+import com.sap.cds.CdsResult;
+import com.sap.cds.ql.CQL;
 import com.sap.cds.ql.Select;
-import com.sap.cds.ql.cqn.CqnAnalyzer;
-
-import com.sap.cds.reflect.CdsModel;
-
 import cds.gen.sap.capire.orders.api.ordersservice.Orders;
+import cds.gen.sap.capire.orders.api.ordersservice.OrdersService;
+import cds.gen.sap.capire.orders.api.ordersservice.Orders.Items;
 import cds.gen.sap.capire.orders.api.ordersservice.Orders_;
+import cds.gen.sap.capire.orders.api.ordersservice.Orders_.Items_;
 import cds.gen.sap.capire.orders.api.ordersservice.OrderChanged;
 import cds.gen.sap.capire.orders.api.ordersservice.OrderChangedContext;
 import cds.gen.sap.capire.orders.api.ordersservice.OrdersService_;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 @ServiceName(OrdersService_.CDS_NAME)
 public class OrdersHandler implements EventHandler {
 
   @Autowired
-  @Qualifier(OrdersService_.CDS_NAME)
-  CqnService service;
-
-  private final CqnAnalyzer analyzer;
-
-  public OrdersHandler(CdsModel model) {
-    this.analyzer = CqnAnalyzer.create(model);
-  }
+  OrdersService service;
 
   @Before(event = CqnService.EVENT_UPDATE)
-  public void beforeUpdateOrders(EventContext context, Stream<Orders> orders) {
-    orders.forEach(order -> {
-      Orders oldOrder = service.run(Select
-        .from(OrdersService_.ORDERS)
-        .columns(o -> o._all(), o -> o.Items().expand())
-        .where(o -> o.ID().eq(order.getId())))
-        .single(Orders.class);
-      List<Orders.Items> oldItems = oldOrder.getItems();
-      for(Orders.Items oldItem : oldItems) {
-        List<Orders.Items> newItems = order.getItems();
-        for(Orders.Items newItem : newItems) {
-          if(oldItem.getId().equals(newItem.getId())) {
-            if(oldItem.getProductId().equals(newItem.getProductId())) {
-              sendOrderChanged(oldItem.getProductId(), newItem.getQuantity() - oldItem.getQuantity());
-            } else {
-              throw new ServiceException(ErrorStatuses.BAD_REQUEST, "ProductId was changed, "+oldItem.getProductId()+" != "+newItem.getProductId()).messageTarget("ProductId");
-            }
-          }
-        }
+  public void beforeUpdateOrders(EventContext context, Orders order) {
+    List<Items> newItems = order.getItems();
+    Set<String> productIds = newItems.stream().map(i -> i.getProductId()).collect(Collectors.toSet());
+    Map<String, Items> itemMap = newItems.stream().collect(Collectors.toMap(i -> i.getProductId(), i -> i));
+
+    CdsResult<Items> beforeImage = service.run(Select.from(Items_.class)
+      .columns(i -> i.quantity(), i -> i.product_ID())
+      .where(i ->
+        i.product_ID().in(productIds)
+        .and(CQL.get("up__ID").eq(order.getId()))));
+
+    for (Items item : beforeImage.listOf(Items.class)) {
+      Items newItem = itemMap.get(item.getProductId());
+      Integer deltaQuantity = newItem.getQuantity() - item.getQuantity();
+      if (deltaQuantity != 0) {
+        sendOrderChanged(item.getProductId(), deltaQuantity);
       }
-    });
+    }
   }
 
-  @Before(event = CqnService.EVENT_DELETE, entity = Orders_.CDS_NAME)
-  public void beforeDeleteOrders(CdsDeleteEventContext context) {
-    String orderId = (String) analyzer.analyze(context.getCqn()).targetKeys().get(Orders.ID);
-    Orders order = service.run(Select.from(OrdersService_.ORDERS)
-      .columns(o -> o._all(), o -> o.Items().expand())
-      .where(o -> o.ID().eq(orderId)))
-      .single(Orders.class);
-    List<Orders.Items> items = order.getItems();
-    for(Orders.Items item : items) {
+  @Before(event = CqnService.EVENT_DELETE)
+  public void beforeDeleteOrders(Orders_ ordersRef) {
+    List<Items> items = service.run(Select.from(ordersRef.Items())
+      .columns(i -> i.product_ID(), i -> i.quantity())).listOf(Items.class);
+
+    for(Items item : items) {
       sendOrderChanged(item.getProductId(), -item.getQuantity());
     }
   }
